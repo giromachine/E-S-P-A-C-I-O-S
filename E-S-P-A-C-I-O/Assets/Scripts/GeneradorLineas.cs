9a482d2b10dcb465125d6c01641e6eabeb8dc1c6
Sebastian Rojas Salazar <69915214+Sreddx@users.noreply.github.com>
2020-08-29 18:56:36 -0500

    Scripts de generacion de lineas. El mejorado es el lineScript.

---
+++ E-S-P-A-C-I-O/Assets/Scripts/GeneradorLineas.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class GeneradorLineas : MonoBehaviour
+{
+    public GameObject lineGen;
+    Lineas linea;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Vector3 dondeGen = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0);
+            GameObject lineaActual = Instantiate(lineGen, dondeGen, transform.rotation);
+            linea = lineaActual.GetComponent<Lineas>();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            linea = null;
+        }
+
+        if(linea != null)
+        {
+
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            linea.DibujarLinea(mousePos);
+        }
+    }
+}
---
+++ E-S-P-A-C-I-O/Assets/Scripts/LineScript2.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LineScript2 : MonoBehaviour
+{
+    private LineRenderer linea2;
+    private Vector3 mousePos;
+    public Material material;
+    private int lineaActual = 0;
+    private Vector3 startMousePos;
+    [SerializeField]
+    private Text distText;
+    private float distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        { 
+            if (linea2 == null)
+            {
+                crearLinea();
+            }
+            startMousePos= Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            startMousePos.z = 0;
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+            linea2.SetPosition(0, mousePos);
+            linea2.SetPosition(1, mousePos);
+        } else if (Input.GetMouseButtonUp(0) && linea2)
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+            linea2.SetPosition(1, mousePos);
+            linea2 = null;
+            lineaActual++;
+        } else if (Input.GetMouseButton(0) && linea2)
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+            linea2.SetPosition(1, mousePos);
+            distance = (mousePos - startMousePos).magnitude;
+            distText.text = distance.ToString("F2") + " metros";
+        }
+        
+    
+    }
+
+    void crearLinea()
+    {
+        linea2 = new GameObject("Linea" + lineaActual).AddComponent<LineRenderer>();
+        linea2.material = material;
+        linea2.positionCount = 2;
+        linea2.startWidth = 0.15f;
+        linea2.endWidth = 0.15f;
+        linea2.useWorldSpace = true;
+        linea2.numCapVertices = 50;
+    }
+    
+
+
+
+}
