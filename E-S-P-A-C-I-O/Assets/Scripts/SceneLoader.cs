a1b9e62054f305cfafb34a43a46934c866d7a07e
capn3m0 <a01630843@itesm.mx>
2020-08-30 09:55:40 -0500

    hola >D

--- E-S-P-A-C-I-O/Assets/Scripts/SceneLoader.cs
+++ E-S-P-A-C-I-O/Assets/Scripts/SceneLoader.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    [SerializeField] int[] scenseChooser;
+    [SerializeField] int[] SceneLoader.scenseChooser();
 
     // Start is called before the first frame update
     void Start()
---
+++ E-S-P-A-C-I-O/Assets/Scripts/barrita.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class barrita : MonoBehaviour {
+
+    public string busqueda;
+    public GameObject inputField;
+    public GameObject textDisplay;
+
+    public void barrita.StoreName()
+    {
+        busqueda = inputField.GetComponent<Text>().text;
+        textDisplay.GetComponent<Text>().text = "No hay coincidencias con" + busqueda;
+    }
+}
