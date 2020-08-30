0450d6cb541adc7194784d14dc4cbe15838f1b55
Carlos Navarro <elgyro99@gmail.com>
2020-08-30 00:42:04 -0500

    Scenes Organized

--- E-S-P-A-C-I-O/Assets/Scripts/RestData.cs
+++ E-S-P-A-C-I-O/Assets/Scripts/RestData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class RestData : MonoBehaviour
 {
@@ -21,32 +22,66 @@ public class RestData : MonoBehaviour
     public GameObject inputFieldName;
     public GameObject inputFieldDirec;
     public GameObject inputFieldTel;
-    public GameObject Dom;
-    public GameObject Lun;
-    public GameObject Mar;
-    public GameObject Mier;
-    public GameObject Jue;
-    public GameObject Vie;
-    public GameObject Sab;
-    public GameObject IsOnService;
+   
 
     public GameObject textDisplay;
-  
-    public void StoreMailandPass()
+
+    private void Awake()
+    {
+        int restDataCount = FindObjectsOfType<RestData>().Length;
+        if(restDataCount > 1)
+        {
+            DestroyRestData();
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void DestroyRestData()
+    {
+        gameObject.SetActive(false);
+        Destroy(gameObject);
+    }
+
+    public void StoreRestInfo()
     {
         Name = inputFieldName.GetComponent<TextMeshProUGUI>().text;
         Direc = inputFieldDirec.GetComponent<TextMeshProUGUI>().text;
         Tel = inputFieldTel.GetComponent<TextMeshProUGUI>().text;
     }
-    public void StoreDaysAndService()
+    public void Dom(bool tog)
     {
-        D = Dom.GetComponent<Toggle>();
-        L = Lun.GetComponent<Toggle>();
-        Ma = Mar.GetComponent<Toggle>();
-        Mi = Mier.GetComponent<Toggle>();
-        J = Jue.GetComponent<Toggle>();
-        V = Vie.GetComponent<Toggle>();
-        S = Sab.GetComponent<Toggle>();
-        OnService = IsOnService.GetComponent<Toggle>();
+        D = tog;
     }
+    public void Lun(bool tog)
+    {
+        L = tog;
+    }
+    public void Mar(bool tog)
+    {
+        Ma = tog;
+    }
+    public void Mier(bool tog)
+    {
+        Mi = tog;
+    }
+    public void Jue(bool tog)
+    {
+        J = tog;
+    }
+    public void Vier(bool tog)
+    {
+        V = tog;
+    }
+    public void Sab(bool tog)
+    {
+        S = tog;
+    }
+    public void InService(bool tog)
+    {
+        OnService = tog;
+    }
+
 }
--- E-S-P-A-C-I-O/Assets/Scripts/SceneLoader.cs
+++ E-S-P-A-C-I-O/Assets/Scripts/SceneLoader.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    [SerializeField] int[] Scene;
+    [SerializeField] int[] scenseChooser;
 
     // Start is called before the first frame update
     void Start()
@@ -15,21 +15,21 @@ public class SceneLoader : MonoBehaviour
     public void LoadNextScene1()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(Scene[0]);
+        SceneManager.LoadScene(scenseChooser[0]);
     }
     public void LoadNextScene2()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(Scene[1]);
+        SceneManager.LoadScene(scenseChooser[1]);
     }
     public void LoadNextScene3()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(Scene[2]);
+        SceneManager.LoadScene(scenseChooser[2]);
     }
     public void LoadNextScene4()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(Scene[3]);
+        SceneManager.LoadScene(scenseChooser[3]);
     }
 }
